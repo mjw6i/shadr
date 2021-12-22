829ee4b310ff289f590d8d8e029d2c5c0c56b096
mjw6i <w6i@tuta.io>
2021-12-22 09:16:03 +0100

    implementation

--- app.go
+++ app.go
@@ -8,34 +8,46 @@ import (
 
 const shaderSource = `
 	#version 460
-	layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+	layout(local_size_x = 3, local_size_y = 1, local_size_z = 1) in;
 	layout(std430, binding = 3) buffer layoutName
 	{
 		uint data_SSBO[];
 	};
+	void compare_and_swap(uint a, uint b) {
+		if (data_SSBO[a] > data_SSBO[b] + 1) {
+			int max = data_SSBO[a] / 20;
+			int diff = (data_SSBO[a] - data_SSBO[b]) / 2;
+			int t = (diff > max) ? max : diff;
+			data_SSBO[a] -= t;
+			data_SSBO[b] += t;
+		}
+	}
 	void main() {
-		data_SSBO[gl_GlobalInvocationID.x] *= 8;
+		if (gl_LocalInvocationID.x >= 1 ) {
+			compare_and_swap(gl_GlobalInvocationID.x, gl_GlobalInvocationID.x - 1);
+		}
+		if (gl_LocalInvocationID.x < 3 - 1 ) {
+			compare_and_swap(gl_GlobalInvocationID.x, gl_GlobalInvocationID.x + 1);
+		}
 	}
 ` + "\x00"
 
 const uint32size = 4
 
 func app() {
-	numbers := []uint32{1, 2, 3, 4, 5}
-	fmt.Println(numbers)
+	frame := []uint32{58, 83, 43}
+	fmt.Println(frame)
 
 	var ssbo uint32
 	gl.GenBuffers(1, &ssbo)
 	gl.BindBuffer(gl.SHADER_STORAGE_BUFFER, ssbo)
-	gl.BufferData(gl.SHADER_STORAGE_BUFFER, len(numbers)*uint32size, gl.Ptr(numbers), gl.DYNAMIC_READ) // DYNAMIC_READ is expected usage for optimisation purposes
+	gl.BufferData(gl.SHADER_STORAGE_BUFFER, len(frame)*uint32size, gl.Ptr(frame), gl.DYNAMIC_READ) // DYNAMIC_READ is expected usage for optimisation purposes
 	gl.BindBufferBase(gl.SHADER_STORAGE_BUFFER, 3, ssbo)
 	gl.BindBuffer(gl.SHADER_STORAGE_BUFFER, 0) // unbind (wiki example, code works without defer here)
 
-	gl.DispatchCompute(uint32(len(numbers)), 1, 1)
-	gl.GetNamedBufferSubData(ssbo, 0, len(numbers)*uint32size, gl.Ptr(numbers))
-	fmt.Println(numbers)
-
-	gl.DispatchCompute(uint32(len(numbers)), 1, 1)
-	gl.GetNamedBufferSubData(ssbo, 0, len(numbers)*uint32size, gl.Ptr(numbers))
-	fmt.Println(numbers)
+	for i := 1; i <= 10; i++ {
+		gl.DispatchCompute(uint32(len(frame)), 1, 1)
+		gl.GetNamedBufferSubData(ssbo, 0, len(frame)*uint32size, gl.Ptr(frame))
+		fmt.Println(frame)
+	}
 }
