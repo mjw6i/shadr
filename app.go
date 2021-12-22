dd794b406447eebe80dd53c6c56ba411bd27e1b5
mjw6i <w6i@tuta.io>
2021-12-22 09:27:33 +0100

    longer input array

--- app.go
+++ app.go
@@ -8,7 +8,7 @@ import (
 
 const shaderSource = `
 	#version 460
-	layout(local_size_x = 3, local_size_y = 1, local_size_z = 1) in;
+	layout(local_size_x = 16, local_size_y = 1, local_size_z = 1) in;
 	layout(std430, binding = 3) buffer layoutName
 	{
 		uint data_SSBO[];
@@ -26,7 +26,7 @@ const shaderSource = `
 		if (gl_LocalInvocationID.x >= 1 ) {
 			compare_and_swap(gl_GlobalInvocationID.x, gl_GlobalInvocationID.x - 1);
 		}
-		if (gl_LocalInvocationID.x < 3 - 1 ) {
+		if (gl_LocalInvocationID.x < 16 - 1 ) {
 			compare_and_swap(gl_GlobalInvocationID.x, gl_GlobalInvocationID.x + 1);
 		}
 	}
@@ -35,8 +35,13 @@ const shaderSource = `
 const uint32size = 4
 
 func app() {
-	frame := []uint32{58, 83, 43}
-	fmt.Println(frame)
+	frame := []uint32{
+		58, 83, 43, 72,
+		06, 18, 34, 90,
+		54, 89, 16, 44,
+		57, 53, 73, 87,
+	}
+	fmt.Println(frame, sum(frame))
 
 	var ssbo uint32
 	gl.GenBuffers(1, &ssbo)
@@ -45,9 +50,18 @@ func app() {
 	gl.BindBufferBase(gl.SHADER_STORAGE_BUFFER, 3, ssbo)
 	gl.BindBuffer(gl.SHADER_STORAGE_BUFFER, 0) // unbind (wiki example, code works without defer here)
 
-	for i := 1; i <= 10; i++ {
+	for i := 1; i <= 50; i++ {
 		gl.DispatchCompute(uint32(len(frame)), 1, 1)
 		gl.GetNamedBufferSubData(ssbo, 0, len(frame)*uint32size, gl.Ptr(frame))
-		fmt.Println(frame)
+		fmt.Println(frame, sum(frame))
 	}
+}
+
+func sum(arr []uint32) int {
+	s := 0
+	for _, e := range arr {
+		s += int(e)
+	}
+
+	return s
 }
